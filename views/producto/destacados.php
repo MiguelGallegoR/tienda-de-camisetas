cfb7a2ffbd77482175195f22c6271fb99c006c94
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-15 15:51:54 +0200

    creado la vista producto con el html para productos destacados

---
+++ views/producto/destacados.php
@@ -0,0 +1,28 @@
+
+
+    <h1>Productos destacados</h1>
+    <div class="product">
+        <img src="assets/img/camiseta.png" alt="">
+        <h2>Camiseta Azul Olgada Ancha</h2>
+        <p>30 euros</p>
+        <a href="" class="button">Comprar</a>
+    </div>
+    <div class="product">
+        <img src="assets/img/camiseta.png" alt="">
+        <h2>Camiseta Azul Olgada Ancha</h2>
+        <p>30 euros</p>
+        <a href="" class="button">Comprar</a>
+    </div>
+    <div class="product">
+        <img src="assets/img/camiseta.png" alt="">
+        <h2>Camiseta Azul Olgada Ancha</h2>
+        <p>30 euros</p>
+        <a href="" class="button">Comprar</a>
+    </div>
+    <div class="product">
+        <img src="assets/img/camiseta.png" alt="">
+        <h2>Camiseta Azul Olgada Ancha</h2>
+        <p>30 euros</p>
+        <a href="" class="button">Comprar</a>
+    </div>
+
