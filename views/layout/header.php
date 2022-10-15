840f57ed504b691e064bb1b24dcdc8166d2846a3
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-15 15:32:25 +0200

    creado el archivo que contiene la cabecera

---
+++ views/layout/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Tienda de Camisetas</title>
+</head>
+<body>
+    <div id="container">
+        <!-- CABECERA-->
+            <header id="header">
+                <div id="logo">
+                    <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                    <a href="index.php">
+                        Tienda de camisetas 
+                    </a>
+                </div>
+            </header>
+
+        <!-- MENU-->
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href="#">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoría 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoría 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoría 3</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoría 4</a>
+                    </li>
+                    <li>
+                        <a href="#">Categoría 5</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div id="content">
