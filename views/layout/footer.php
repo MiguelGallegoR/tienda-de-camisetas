bf3dcc13ff245381ce7ff58c5410aaff0d24eb2c
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-15 15:33:19 +0200

    creo el archivo que contiene el footer

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+            </div>
+
+            <!-- PIE DE PAGINA-->
+            <footer id="footer">
+                <p>Desarrollado por Miguel Gallego Rodríguez &copy; <?= date('Y') ?></p>
+            </footer>
+
+        </div>
+    </body>
+</html>
