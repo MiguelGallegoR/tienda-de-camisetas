c0309cf0285c57bf1a35f0b6e327a9d8e2734c95
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-16 12:25:02 +0200

    modificado header para que cargue los estilos con la url base

--- views/layout/header.php
+++ views/layout/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
     <title>Tienda de Camisetas</title>
 </head>
 <body>
@@ -12,7 +12,7 @@
         <!-- CABECERA-->
             <header id="header">
                 <div id="logo">
-                    <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+                    <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo">
                     <a href="index.php">
                         Tienda de camisetas 
                     </a>
