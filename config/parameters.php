142bd8848c61c21d1cbff04ba73e81899f30705e
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-16 12:23:18 +0200

    creado archivo con los parametros de ruta amigables

---
+++ config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+
+define("base_url", "http://localhost/master-php/proyecto-php-poo/tienda-de-camisetas/");
+
+define("controller_default", "productoController");
+
+define("action_default", "index");
