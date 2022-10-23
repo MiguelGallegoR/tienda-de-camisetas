babb6442c4ef94b505b626865987712b15944863
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-23 13:32:08 +0200

    creada vista para guardar producto

---
+++ views/producto/crear.php
@@ -0,0 +1,35 @@
+<h1>Crear nuevos productos</h1>
+
+<div class="form_container">
+    <form action="<?=base_url?>producto/save" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" required>
+
+        <label for="descripcion">Descripcion</label>
+        <textarea name="descripcion"></textarea>
+
+        <label for="precio">Precio</label>
+        <input type="text" name="precio" required>
+
+        <label for="stock">Stock</label>
+        <input type="number" name="stock" required>
+
+        <label for="categoria">Categoria</label>
+        <?php $categorias = Utils:: showCategorias(); ?>
+        <select name="categoria">
+            <?php while($cat = $categorias->fetch_object()) : ?>
+                <option value="<?=$cat->id?>">
+                    <?=$cat->nombre?>
+                </option>
+            <?php endwhile; ?> 
+        </select>
+
+        <label for="imagen">Imagen</label>
+        <input type="file" name="imagen" required>
+
+
+        <input type="submit" value="Guardar">
+
+
+    </form>
+</div>
