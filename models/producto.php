79274fdc0a5167e3a17360c5279cb0855ac6859f
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-23 12:14:07 +0200

    creado modelo de producto

---
+++ models/producto.php
@@ -0,0 +1,97 @@
+<?php
+
+class Producto{
+    private $id;
+    private $categoria_id;
+    private $nombre;
+    private $descripcion;
+    private $precio;
+    private $stock;
+    private $oferta;
+    private $fecha; 
+    private $imagen; 
+    
+    public function __construct(){
+       $this-> db = Database::connect();
+    }
+
+    function getId(){
+        return $this->id;
+    }
+
+    function getCategoriaId(){
+        return $this->categoria_id;
+    }
+
+    function getNombre(){
+        return $this->nombre;
+    }
+    
+    function getDescripcion(){
+        return $this->descripcion;
+    }
+
+    function getPrecio(){
+        return $this->precio;
+    }
+
+    function getStock(){
+        return $this->stock;
+    }
+
+    function getOferta(){
+        return $this->oferta;
+    }
+
+    function getFecha(){
+        return $this->fecha;
+    }
+
+
+    function getImagen(){
+        return $this->imagen;
+    }
+
+    function setId($id){
+        $this->id = $id;
+    }
+
+    function setCategoriaId($categoria_id){
+        $this->categoria_id = $categoria_id;
+    }
+
+    function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    function setDescripcion($descripcion){
+        $this->descripcion = $descripcion;
+    }
+
+    function setPrecio($precio){
+        $this->precio = $precio;
+    }
+
+    function setStock($stock){
+        $this->stock = $stock;
+    }
+
+    function setOferta($oferta){
+        $this->oferta = $oferta;
+    }
+
+    function setFecha($fecha){
+        $this->fecha = $fecha;
+    }
+
+
+    function setImagen($fecha){
+        $this->imagen = $fecha;
+    }
+
+    public function getAll(){
+        $productos = $this->db->query("SELECT*FROM productos");
+       return $productos;
+    }
+
+}    
