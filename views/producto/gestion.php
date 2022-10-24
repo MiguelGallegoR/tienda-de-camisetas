e0367eb4f53dbb77005bd6bd3949eb10af3e6305
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-24 16:59:05 +0200

    añadido condicion con la sesion producto

--- views/producto/gestion.php
+++ views/producto/gestion.php
@@ -2,6 +2,15 @@
 <a href="<?=base_url?>producto/crear" class="button button-small">
     Crear producto
 </a>
+
+<?php if(isset($_SESSION['producto']) && $_SESSION['producto'] == 'complete'): ?>
+   <strong class="alert_green">Producto creado correctamente</strong>
+
+<?php elseif(isset($_SESSION['producto']) && $_SESSION['producto'] == 'failed'): ?>
+    <strong class="alert_red">Producto fallido al crear</strong>
+<?php endif; ?>
+<?php Utils::deleteSession('producto'); ?>
+
 <table>
     <tr>
         <th>ID</th>
