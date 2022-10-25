8102fdcd4a66086fdb93979245c78d068004da0c
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-25 17:34:07 +0200

    actualizado metodo save para que guarde los editados tambien

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -38,19 +38,30 @@ class productoController{
                 $producto->setStock($stock);
                 $producto->setCategoriaId($categoria);
 
-                $file = $_FILES['imagen'];
-                $filename = $file['name'];
-                $mimetype = $file['type'];
+                if(isset($_FILES['imagen'])){
+                    $file = $_FILES['imagen'];
+                    $filename = $file['name'];
+                    $mimetype = $file['type'];
 
-                if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png' || $mimetype == 'image/gif'){
-                    if(!is_dir('uploads/images')){
-                        mkdir('uploads/images', 0777, true);
+                    if($mimetype == "image/jpg" || $mimetype == 'image/jpeg' || $mimetype == 'image/png' || $mimetype == 'image/gif'){
+                        if(!is_dir('uploads/images')){
+                            mkdir('uploads/images', 0777, true);
+                        }
+                        $producto->setImagen($filename);
+                        move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
                     }
-                    $producto->setImagen($filename);
-                    move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
                 }
 
-                $save = $producto->save();
+                if(isset($_GET['id'])){
+                    $id = $_GET['id'];
+                    $producto->setId($id);
+
+                    $save = $producto->edit();
+                }else{
+                    $save = $producto->save();
+                }
+               
+
                 if($save){
                     $_SESSION['producto'] = 'complete';
                 }else{
@@ -67,9 +78,16 @@ class productoController{
 
     public function editar(){
         Utils::isAdmin();
-        $edit = true;
-        require_once 'views/producto/crear.php';
-
+        if(isset($_GET['id'])){
+            $edit = true;
+            $id = $_GET['id'];
+            $producto = new Producto();
+            $producto->setId($id);
+            $pro = $producto->getOne();
+            require_once 'views/producto/crear.php';
+        }else{
+            header('Location:'.base_url.'producto/gestion');
+        }
     }
 
     public function eliminar(){
