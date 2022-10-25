690200bb0163a83738e1b3d7d8d5323d9d1dafeb
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-25 16:26:44 +0200

    añadido condicion si existe sesiondelete

--- views/producto/gestion.php
+++ views/producto/gestion.php
@@ -7,7 +7,15 @@
    <strong class="alert_green">Producto creado correctamente</strong>
 
 <?php elseif(isset($_SESSION['producto']) && $_SESSION['producto'] == 'failed'): ?>
-    <strong class="alert_red">Producto fallido al crear</strong>
+    <strong class="alert_red">Fallo al crear producto</strong>
+<?php endif; ?>
+<?php Utils::deleteSession('producto'); ?>
+
+<?php if(isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete'): ?>
+   <strong class="alert_green">Producto eliminado correctamente</strong>
+
+<?php elseif(isset($_SESSION['delete']) && $_SESSION['delete'] == 'failed'): ?>
+    <strong class="alert_red">Fallo al eliminar producto</strong>
 <?php endif; ?>
 <?php Utils::deleteSession('producto'); ?>
 
@@ -18,6 +26,7 @@
         <th>NOMBRE</th>
         <th>PRECIO</th>
         <th>STOCK</th>
+        <th>Acciones</th>
        
     </tr>
 <?php while($pro = $productos->fetch_object()): ?>
@@ -27,6 +36,10 @@
         <td><?=$pro->nombre;?></td>
         <td><?=$pro->precio;?></td>
         <td><?=$pro->stock;?></td>
+        <td>
+            <a href="<?=base_url?>producto/editar&id=<?=$pro->id?>" class="button button-gestion">Editar</a>
+            <a href="<?=base_url?>producto/eliminar&id=<?=$pro->id?>" class="button button-gestion button-red">Eliminar</a>
+        </td>
     </tr>
 <?php endwhile; ?>
 </table>
