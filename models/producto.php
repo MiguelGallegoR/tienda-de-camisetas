20c91d4889265cae44472f21fa10a1afa572af49
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-25 17:31:47 +0200

    añadidos los metodos getOne y Edit

--- models/producto.php
+++ models/producto.php
@@ -94,6 +94,11 @@ class Producto{
        return $productos;
     }
 
+    public function getOne(){
+        $producto = $this->db->query("SELECT*FROM productos WHERE id ={$this->getId()}");
+       return $producto->fetch_object();
+    }
+
     public function save(){
         $sql = "INSERT INTO productos VALUES (NULL, {$this->getCategoriaId()} ,'{$this->getNombre()}', '{$this->getDescripcion()}', {$this->getPrecio()}, {$this->getStock()}, NULL, CURDATE() , '{$this->getImagen()}');";
         $save = $this->db->query($sql);
@@ -107,6 +112,23 @@ class Producto{
     }
 
 
+    public function edit(){
+        $sql = "UPDATE productos SET nombre ='{$this->getNombre()}', descripcion ='{$this->getDescripcion()}', precio ={$this->getPrecio()}, stock ={$this->getStock()}, categoria_id ={$this->getCategoriaId()} ";
+        if($this->getImagen() !== null){
+            $sql .= ", imagen ='{$this->getImagen()}'";
+        }
+        
+        $sql .= "WHERE id ={$this->getId()};";
+
+        $save = $this->db->query($sql);
+
+    
+        $result = false;
+        if($save){
+            $result = true;
+        }
+        return $result;
+    }
 
     public function delete(){
         $sql = "DELETE FROM productos WHERE id={$this->id}";
