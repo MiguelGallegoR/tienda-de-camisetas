65612127d8dde88c998a6a51fb77fcda9552e200
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-10-27 17:02:56 +0200

    cambiado orden parrafos

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -10,9 +10,9 @@
         </div>
                 
         <div class="data">
-            <p class="price"><?=$pro->precio?>$</p>
             <p class="description"><?=$pro->descripcion?></p>
-            <a href="" class="button">Comprar</a>
+            <p class="price"><?=$pro->precio?>$</p>
+            <a href="<?=base_url?>carrito/add&id=<?=$pro->id?>" class="button">Comprar</a>
         </div>
     </div>
 
