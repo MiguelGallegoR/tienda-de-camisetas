ea5106e680a210d884f2c9612babf4568dd4e253
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-03 12:48:25 +0100

    cambio en el nombre de pro a product

--- views/producto/ver.php
+++ views/producto/ver.php
@@ -1,21 +1,21 @@
-<?php if(isset($pro)) : ?>
-    <h1><?= $pro->nombre ?></h1>
+<?php if(isset($product)): ?>
+    <h1><?=$product->nombre?></h1>
     <div id="detail-product">
         <div class="image">
-            <?php if($pro->imagen != null) : ?>
-                <img src="<?=base_url?>uploads/images/<?=$pro->imagen?>" >
+            <?php if($product->imagen != null) : ?>
+                <img src="<?=base_url?>uploads/images/<?=$product->imagen?>" >
             <?php else : ?>
                 <img src="<?=base_url?>assets/img/camiseta.png" alt="">
             <?php endif ; ?>    
         </div>
                 
         <div class="data">
-            <p class="description"><?=$pro->descripcion?></p>
-            <p class="price"><?=$pro->precio?>$</p>
-            <a href="<?=base_url?>carrito/add&id=<?=$pro->id?>" class="button">Comprar</a>
+            <p class="description"><?=$product->descripcion?></p>
+            <p class="price"><?=$product->precio?>$</p>
+            <a href="<?=base_url?>carrito/add&id=<?=$product->id?>" class="button">Comprar</a>
         </div>
     </div>
 
-<?php else: ?>
+<?php else:?>
     <h1>El producto no existe</h1>
 <?php endif; ?>
