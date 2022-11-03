d455fcb06ddfa24a84f8ff02cd90631190ac972a
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-03 12:46:47 +0100

    añadido ob_start

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,4 +1,5 @@
 <!-- BARRA LATERAL-->
+<?php ob_start(); ?>
 <aside id="lateral">
     <div id="carrito" class="block_aside">
         <h3>Mi carrito</h3>
@@ -28,15 +29,13 @@
             
             <?php if(isset($_SESSION['admin'])):?>
                 <li><a href="<?=base_url?>categoria/index">Gestionar categorias</a></li>
-                <li><a href="<?=base_url?>producto/gestion"">Gestionar productos</a></li>
+                <li><a href="<?=base_url?>producto/gestion">Gestionar productos</a></li>
                 <li><a href="#">Gestionar pedidos</a></li>
-                
             <?php endif;?>
 
             <?php if(isset($_SESSION['identity'])):?>
                 <li><a href="#">Mis pedidos</a></li>
                 <li><a href="<?=base_url?>usuario/logout">Cerrar sesión </a></li>
-            
             <?php else:?>
                 <li><a href="<?=base_url?>usuario/registro">Registrate aquí</a></li>
             <?php endif;?>
