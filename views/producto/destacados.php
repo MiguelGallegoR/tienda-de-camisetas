237b068fd95a5721b5d64dd71854105b6198a99c
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-03 12:47:25 +0100

    eliminado / del enlace product

--- views/producto/destacados.php
+++ views/producto/destacados.php
@@ -3,7 +3,7 @@
     
     <?php while($product = $productos->fetch_object()) : ?>
         <div class="product">
-        <a href="<?=base_url?>/producto/ver&id=<?=$product->id?>">
+        <a href="<?=base_url?>producto/ver&id=<?=$product->id?>">
             <?php if($product->imagen != null) : ?>
                 <img src="<?=base_url?>uploads/images/<?=$product->imagen?>" >
             <?php else : ?>
