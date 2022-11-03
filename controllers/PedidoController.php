380e318c313e84104ed3b06d919cae188a56498a
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-03 17:03:20 +0100

    actualizado index y creado metodo add

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -1,7 +1,40 @@
 <?php
-
+require_once 'models/pedido.php';
 class pedidoController{
-    public function index(){
-        echo 'Controlador Pedido, Accion Index';
+    public function hacer(){
+        require_once 'views/pedido/hacer.php';
+
+    }
+
+    public function add(){
+        if(isset($_SESSION['identity'])){
+            $usuario_id = $_SESSION['identity']->id;
+            $provincia = isset($_POST['provincia']) ? $_POST['provincia'] : false;
+            $localidad = isset($_POST['localidad']) ? $_POST['localidad'] : false;
+            $direccion = isset($_POST['direccion']) ? $_POST['direccion'] : false;
+
+            $stats = Utils::statsCarrito();
+            $coste = $stats['total'];
+
+            if($provincia && $direccion && $localidad){
+                $pedido = new Pedido();
+                $pedido->setUsuarioId($usuario_id);
+                $pedido->setProvincia($provincia);
+                $pedido->setLocalidad($localidad);
+                $pedido->setDireccion($direccion);
+                $pedido->setCoste($coste);
+
+                $save = $pedido->save();
+                if($save){
+                    $_SESSION['pedido'] = 'completed';
+                }else{
+                    $_SESSION['pedido'] = 'failed';
+                }
+            }
+        }else{
+            header("Location:".base_url);
+        }
+
+       
     }
 }
