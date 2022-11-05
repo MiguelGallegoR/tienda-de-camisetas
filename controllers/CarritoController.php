a96fe096b9e3d99b328bd1ef912d86492117d8da
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-05 16:47:25 +0100

    creados metodos remove, up y down

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -2,8 +2,12 @@
 require_once 'models/producto.php';
 class carritoController{
     public function index(){
+        if(isset($_SESSION['carrito']) && count($_SESSION['carrito']) >=1){
+            $carrito = $_SESSION['carrito'];
+        }else{
+            $carrito = array();
+        }
         
-        $carrito = $_SESSION['carrito'];
         require_once 'views/carrito/index.php';
     }  
 
@@ -45,7 +49,30 @@ class carritoController{
 
 
     public function remove(){
+        if(isset($_GET['index'])){
+            $index = $_GET['index'];
+            unset($_SESSION['carrito'][$index]);
+        }
+        header("Location:".base_url."carrito/index");
+    }
 
+    public function up(){
+        if(isset($_GET['index'])){
+            $index = $_GET['index'];
+           $_SESSION['carrito'][$index]['unidades']++;
+        }
+        header("Location:".base_url."carrito/index");
+    }
+
+    public function down(){
+        if(isset($_GET['index'])){
+            $index = $_GET['index'];
+            $_SESSION['carrito'][$index]['unidades']--;
+            if($_SESSION['carrito'][$index]['unidades'] == 0){
+                unset($_SESSION['carrito'][$index]);
+            }
+        }
+        header("Location:".base_url."carrito/index");
     }
 
     public function delete_all(){
