8408b30ccf9e913725ceb017c0765ad06db1de42
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-05 12:51:41 +0100

    creado form para cambiar estado y añadido estado en datos del pedido

--- views/pedido/detalle.php
+++ views/pedido/detalle.php
@@ -2,6 +2,21 @@
 
 <?php if(isset($pedido)): ?>
 
+    <?php if(isset($_SESSION['admin'])): ?>
+    <h3>Cambiar el estado del pedido</h3>
+    <form action="<?=base_url?>pedido/estado" method="post">
+        <input type="hidden" value="<?=$pedido->id?>" name="pedido_id">
+        <select name="estado">
+            <option value="confirm" <?=$pedido->estado == "confirm" ? "selected" : '';?>>Pendiente</option>
+            <option value="preparation" <?=$pedido->estado == "preparation" ? "selected" : '';?>>En preparacion</option>
+            <option value="ready" <?=$pedido->estado == "ready" ? "selected" : '';?>>Listo para enviar</option>
+            <option value="sended" <?=$pedido->estado == "sended" ? "selected" : '';?>>Enviado</option>
+        </select>
+        <input type="submit" value="Cambiar estado">
+    </form>
+    <br>
+    <?php endif; ?>
+
     <h3>Dirección del envío:</h3>
     
     Provincia: <?=$pedido->provincia?> <br>
@@ -9,7 +24,7 @@
     Dirección: <?=$pedido->direccion?> <br>
 
     <h3>Datos del pedido:</h3>
-    
+    Estado: <?=Utils::showStatus($pedido->estado)?> <br>
     Número de pedido: <?=$pedido->id?> <br>
     Total a pagar: <?=$pedido->coste?> $<br>
     Productos: 
