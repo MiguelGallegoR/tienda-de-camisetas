7d1ed0c681f87f27eaa72dd3cf454218f89e7240
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-05 12:52:04 +0100

    añadido estado en tabla

--- views/pedido/mis_pedidos.php
+++ views/pedido/mis_pedidos.php
@@ -1,10 +1,18 @@
+<?php if(isset($gestion)): ?>
+    <h1>Gestionar pedidos</h1>
+
+<?php else: ?>
+
 <h1>Mis pedidos</h1>
+
+<?php endif; ?>
 
 <table>
     <tr>
         <th>Nº Pedido</th>
         <th>Coste</th>
         <th>Fecha</th>
+        <th>Estado</th>
     </tr>
     <?php while($ped = $pedidos->fetch_object()): ?>
 
@@ -18,7 +26,9 @@
         <td>
             <?=$ped->fecha?>
         </td>
-            
+        <td>
+            <?=Utils::showStatus($ped->estado)?>
+        </td>
     </tr>
 
     <?php endwhile; ?>
