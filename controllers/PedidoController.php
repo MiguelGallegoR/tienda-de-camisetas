b43fbd76952ffd3334453ac49d4cb78a8924e1ec
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2022-11-05 12:50:16 +0100

    creados metodos gestion y estado

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -87,7 +87,32 @@ class pedidoController{
             header("Location:".base_url."pedido/mispedidos");
         }
 
-
-        
     }
+
+    public function gestion(){
+        Utils::isAdmin();
+        $gestion = true;
+        $pedido = new Pedido();
+        $pedidos = $pedido->getAll();
+        require_once 'views/pedido/mis_pedidos.php';
+    }
+
+
+    public function estado(){
+        Utils::isAdmin();
+        if(isset($_POST['pedido_id']) && isset($_POST['estado'])){
+            $estado = $_POST['estado'];
+            $id = $_POST['pedido_id'];
+            $pedido = new Pedido();
+            $pedido->setId($id);
+            $pedido->setEstado($estado);
+            $pedido->edit();
+
+            header("Location:".base_url."pedido/detalle&id=".$id);
+
+        }else{
+            header("Location:".base_url);
+        }
+    }
+
 }
